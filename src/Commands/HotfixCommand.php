50d67473b938820614540e6570b2ffdf5e2e5df9
samirsabiee <samirsabiee@gmail.com>
2023-01-11 20:35:05 +0330

    complete hotfix command

--- src/Commands/HotfixCommand.php
+++ src/Commands/HotfixCommand.php
@@ -4,6 +4,7 @@ namespace SamirSabiee\Hotfix\Commands;
 
 use Exception;
 use Illuminate\Console\Command;
+use SamirSabiee\Hotfix\Hotfix;
 use SamirSabiee\Hotfix\StubManager;
 
 class HotfixCommand extends Command
@@ -16,7 +17,11 @@ class HotfixCommand extends Command
     {
         try {
             $files = glob(app_path('Hotfixes/' . config('hotfix.path')));
-            dd($files);
+            foreach ($files as $file) {
+                /** @var Hotfix $hotfix */
+                $hotfix = resolve("App\\Hotfixes" . str_replace('/', '\\', last(array_reverse(explode('.php', last(explode('app/Hotfixes', $file)))))));
+                $hotfix->run();
+            }
         } catch (Exception $e) {
             $this->error($e->getMessage());
         }
