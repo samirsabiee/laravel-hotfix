2a80d338c664d99d01c3e407783e2b78aa3ce13a
samirsabiee <samirsabiee@gmail.com>
2023-01-12 15:00:24 +0330

    test to catch any type of extension

--- src/Hotfix.php
+++ src/Hotfix.php
@@ -3,6 +3,7 @@
 namespace SamirSabiee\Hotfix;
 
 use Illuminate\Support\Facades\DB;
+use PhpParser\Error;
 use SamirSabiee\Hotfix\Models\Hotfix as HotfixModel;
 
 abstract class Hotfix implements IHotfix
@@ -30,7 +31,7 @@ abstract class Hotfix implements IHotfix
                 'name' => static::class,
                 'error' => null
             ]);
-        } catch (\Exception $e) {
+        } catch (\ParseError | Error | \Exception $e) {
             DB::rollBack();
             HotfixModel::query()->updateOrCreate([
                 'name' => static::class
